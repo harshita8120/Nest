fe02faf0621d16dd3edf014cf6059853e4b7ed79
harshita8120 <harshita.ranjan25@gmail.com>
2026-01-03 18:53:15 +0530

    fix: footer navigation links open in the same tab #3153

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -53,7 +53,7 @@ export default function Footer() {
                       <span className="text-slate-600 dark:text-slate-400">{link.text}</span>
                     ) : (
                       <Link
-                        target="_blank"
+                        target={link.href?.startsWith('http')? '_blank' : '_self'}
                         className="text-slate-600 hover:text-slate-900 dark:text-slate-400 dark:hover:text-slate-100"
                         href={link.href || '/'}
                       >
